aa8a92475c7bd4661323f896ac8d72fc4caed54a
llam19 <sanaya2004@icloud.com>
2025-01-21 19:41:03 -0500

    made fruit salad

---
+++ FruitSaladLibrary/FruitSaladLibrary/Fruit.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FruitSaladLibrary
+{
+    public abstract class Fruit
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+        public double Weight { get; set; }
+        public bool IsOrganic { get; set; }
+
+        public Fruit(string name, string color, double weight, bool isOrganic)
+        {
+            Name = name;
+            Color = color;
+            Weight = weight;
+            IsOrganic = isOrganic;
+        }
+        //abstract method to enforce implementation in derived classes
+        public abstract string Describe();
+
+        public override string ToString()
+        {
+            string organicLabel = IsOrganic ? "Organic" : "Non-Organic";
+            return $"{Name} ({Color}, {Weight}g, {organicLabel})";
+        }
+    }
+}
